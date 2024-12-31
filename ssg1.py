75e5043cf2bbdbd7a14679ee7f0ec879b382e008
cimini-xyz <github@cimini.xyz>
2024-12-30 22:29:20 -0600

    Implemented functions

--- ssg1.py
+++ ssg1.py
@@ -1,8 +1,10 @@
 def format_filename_string(filename_string, article_title):
-    pass
+    slugged_article_title = re.sub(r'[^a-zA-Z0-9 ]','', article_title)
+    filename_string = re.sub(r'\s+','-', slugged_article_title)
+    return filename_string.lower() + ".html"
 
 def update_filename_on_system(file_path, filename_string):
-    pass
+    file_path.source.move(file_path.source.parents[0] / filename_string)
 
 def process_filename(file_path, filename_string, article_title):
     valid_filename_string = format_filename_string(filename_string, article_title)
