1beec056456c0cfb668ff6a9237c2562f3962bf2
cimini-xyz <github@cimini.xyz>
2024-12-30 22:10:52 -0600

    Fixed syntax issues and typos

--- ssg1.py
+++ ssg1.py
@@ -1,4 +1,4 @@
-def format_filename_string(filename_string ,article_title):
+def format_filename_string(filename_string, article_title):
     pass
 
 def update_filename_on_system(file_path, filename_string):
@@ -6,6 +6,6 @@ def update_filename_on_system(file_path, filename_string):
 
 def process_filename(file_path, filename_string, article_title):
     valid_filename_string = format_filename_string(filename, article_title)
-    if not valid_filename_string == file_name_string
+    if not valid_filename_string == filename_string:
         update_filename_on_system(file_path, valid_filename_string)
         
