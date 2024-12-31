40e74d1f4e7c5cafc55d46d88d957c2ff19eed63
cimini-xyz <github@cimini.xyz>
2024-12-30 22:12:30 -0600

    Fixed intended variable

--- ssg1.py
+++ ssg1.py
@@ -5,7 +5,7 @@ def update_filename_on_system(file_path, filename_string):
     pass
 
 def process_filename(file_path, filename_string, article_title):
-    valid_filename_string = format_filename_string(filename, article_title)
+    valid_filename_string = format_filename_string(filename_string, article_title)
     if not valid_filename_string == filename_string:
         update_filename_on_system(file_path, valid_filename_string)
         
