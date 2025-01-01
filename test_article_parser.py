9a602dc80203220111048baae68fb938b5f838eb
cimini-xyz <github@cimini.xyz>
2024-12-31 20:33:59 -0600

    Updated test file

--- test_article_parser.py
+++ test_article_parser.py
@@ -3,10 +3,10 @@ from pathlib import Path
 from shutil import move, copy2
 
 def test_article_parser():
-    html_document = Path('html/Test Document.html')
+    html_document = Path('html/understanding-python-decorators.html')
     html_content = html_document.read_text()
-    parser = ArticleParser()
-    parser.feed(html_content)
+    parser = ArticleParser(html_document)
+    parser.parse()
     print(parser.article_title, parser.published_time, parser.category_type)
     print(format_filename(parser.article_title))
 
